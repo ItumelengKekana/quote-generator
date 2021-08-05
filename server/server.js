5fb505e69097b1f705b759aceabd595f61a102a1
Itumeleng Kekana <kekanaitumeleng209@gmail.com>
2021-08-05 17:13:06 +0200

    initial commit

---
+++ server/server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const port = process.env.port || 3000;
+const publicPath = path.join(__dirname, '..', 'build');
+
+
+app.use(express.static(publicPath));
+
+app.get('*', (req, res) => {
+   res.sendFile(path.join(publicPath, 'index.html'));
+});
+
+app.listen(port, () =>{
+    console.log('server up!')
+});
--- src/App.js
+++ src/App.js
@@ -1,24 +1,53 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState} from 'react';
 
 function App() {
+	const [quote, setQuote] = useState("");
+	const [author, setAuthor] = useState("");
+	const [results, setresults] = useState([]);
+
+	const setBg = () => {
+		const randomColor = Math.floor(Math.random()*16777215).toString(16);
+		document.body.style.backgroundColor = "#" + randomColor;
+
+		const text = document.querySelector('#text');
+		const auth = document.querySelector('#author');
+		const but = document.querySelector('.button');
+
+		text.style.color = '#' + randomColor;
+		auth.style.color = '#' + randomColor;
+		but.style.backgroundColor = '#' + randomColor;
+
+	  }
+
+	const getQuote = async() =>{
+		const endpoint = 'https://goquotes-api.herokuapp.com/api/v1/random?count=1';
+
+		const response = await fetch(endpoint);
+
+		const json = await response.json();
+		console.log(json);
+
+		setresults(json.quotes);
+		setQuote(json.quotes[0].text);
+		setAuthor(json.quotes[0].author);
+
+		setBg();
+	}
+
+	const url = `twitter.com/intent/tweet?hashtags=quotes&text=${quote}-${author}`;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+	<div className="App">
+	  <div className="container" id="quote-box">
+		<div className="quote-text">
+			<p id="text">{quote}</p>
+		</div>
+		<h3 id="author">-{author}</h3>
+		<a href={url} id="tweet-quote" className="button" target="_blank">Tweet</a>
+		<button id="new-quote" className="button" onClick={getQuote}>New Quote</button>
+	  </div>
+	</div>
   );
 }
 
